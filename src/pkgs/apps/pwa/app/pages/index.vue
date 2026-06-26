6f6b831c210492795309c6c74a6753d4e8a94a13
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-06-26 00:50:28 +0000

    feat(apps/pwa): integrate PWA support and Vue Query

---
+++ src/pkgs/apps/pwa/app/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
+
+const getTodos = async () => {
+  return [1, 2, 3]
+}
+
+const { isPending, isError, data, error } = useQuery({
+  queryKey: ['todos'],
+  queryFn: getTodos,
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Olá, Mundo!</h1>
+    <UIButton />
+    <pre><code>{{ { isPending, isError, data, error } }}</code></pre>
+  </div>
+</template>
