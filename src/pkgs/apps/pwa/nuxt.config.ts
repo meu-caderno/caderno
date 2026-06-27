9ba999767cf467d6cff9af6858e950c0bd673f0e
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-06-27 14:19:09 +0000

    chore(nuxt.config): standardize baseURL formatting in configuration

--- src/pkgs/apps/pwa/nuxt.config.ts
+++ src/pkgs/apps/pwa/nuxt.config.ts
@@ -1,4 +1,3 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
@@ -9,7 +8,7 @@ export default defineNuxtConfig({
   },
 
   app: {
-    baseURL: process.env.BASE_URL || '/'
+    baseURL: process.env.BASE_URL || "/",
   },
 
   srcDir: "./app",
